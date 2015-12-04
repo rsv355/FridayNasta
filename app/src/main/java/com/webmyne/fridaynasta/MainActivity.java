c5699418b04452e9c0e14ada2b4d9a62d421a63b
dhruvil <dd.droid@yahoo.com>
2015-12-04 20:28:04 +0530

    Logout

--- app/src/main/java/com/webmyne/fridaynasta/MainActivity.java
+++ app/src/main/java/com/webmyne/fridaynasta/MainActivity.java
@@ -1,8 +1,16 @@
 package com.webmyne.fridaynasta;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +22,59 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setSubtitle("Welcome, "+PrefUtils.getUserName(MainActivity.this));
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.trainer_diary_popup, menu);
+
+        MenuItem item = menu.findItem(R.id.item_logout);
+        Drawable drawable = item.getIcon();
+        drawable.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        item.setIcon(drawable);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()){
+
+            case R.id.item_logout:
+                doLogout();
+
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void doLogout() {
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Logout")
+                .setMessage("Are you sure you want to logout?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+
+                        PrefUtils.saveLogin(MainActivity.this,false);
+                        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+
     }
 }
