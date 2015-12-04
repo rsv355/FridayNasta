0302b885a63d2fcb24bdda48367e8b914397a5f5
APK Provider <dev.apkprovider@gmail.com>
2015-12-04 17:07:29 +0530

    mb

--- app/src/main/java/com/webmyne/fridaynasta/LoginActivity.java
+++ app/src/main/java/com/webmyne/fridaynasta/LoginActivity.java
@@ -1,14 +1,34 @@
 package com.webmyne.fridaynasta;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        init();
+    }
+
+    private void init(){
+        txtLogin = (TextView)findViewById(R.id.txtLogin);
+
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
 
     }
+
+    //end of main class
 }
